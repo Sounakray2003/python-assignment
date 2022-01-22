65d58d01f91728d5bfc1b701ae40e2f87ff651f7
Sounakray2003 <92640729+Sounakray2003@users.noreply.github.com>
2022-01-23 00:45:16 +0530

    Add files via upload

---
+++ opperators.py
@@ -0,0 +1,18 @@
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+Addition = a+b
+print("Addition of two number is", Addition)
+subtraction = a-b
+print("Subtraction of two number is", subtraction)
+Multiplication = a*b
+print("Multiplication of two number is", Multiplication)
+Division = b/a
+print("Division of two number is", Division)
+Floordivision = b//a
+print("Floor division of two number is", Floordivision)
+Exponential1 = a**b
+print("powers of a to b is", Exponential1)
+Exponential2 = b**a
+print("powers of b to a is", Exponential2)
+Modulo = b % a
+print("Remainder left after division of a and b", Modulo)
