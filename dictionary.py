c706a625db166853da823d3d517dd6b9e656a392
Sounakray2003 <92640729+Sounakray2003@users.noreply.github.com>
2022-02-01 20:08:53 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,7 @@
+b={"MAHARASTRA":"MH","WEST BENGAL":"WB","TAMIL NADU":"TN","JAMMU & KASHMIR":"J&K" }
+print(b["MAHARASTRA"])
+print(b["WEST BENGAL"])
+print(b["TAMIL NADU"])
+print(b["JAMMU & KASHMIR"])
+b["UTTAR PRADESH"]="UP"
+print(b)
