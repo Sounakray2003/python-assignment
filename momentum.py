348a6b61d74c3807538c7c20c08ed461ecefd15f
Sounakray2003 <92640729+Sounakray2003@users.noreply.github.com>
2022-02-06 11:23:56 +0530

    save changes

---
+++ momentum.py
@@ -0,0 +1,7 @@
+# Name : Sounak Ray
+# Roll_no :21CO119
+
+m=int(input("Enter mass in kilogram: "))
+c=int(input("Enter velocity in meter per second: " ))
+Momentum=e=m*c**2
+print("Momentum of body in kg.m/s: ",Momentum)
